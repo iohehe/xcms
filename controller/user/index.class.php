84e91138c6225cc446ec505e0d789ef7a5294865
iohehe <iohehe@yeah.net>
2019-10-29 20:37:43 +0800

    v2: view init 2/2

--- controller/user/index.class.php
+++ controller/user/index.class.php
@@ -3,14 +3,7 @@ class index extends base{
 
 
 	public function indexAction(){
-		if (isset($_GET['id']))
-        {
-            $id = (int)$_GET['id'];
-        }
-		else
-        {
-            $this->view->display('index');
-        }
+       $this->view->display('index');
 	}
 
 	public function sayhelloAction(){
